69f9d1cb6baa59257923d6053abf5228109f3601
Gilbert Enriquez <gilbertenriquez10@gmail.com>
2021-09-15 09:48:50 +0800

    Add project files.

---
+++ StackLayout/StackLayout/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StackLayout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        //here just change the mainpage to stacklayout2 if you want to run the stacklayout2
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
